ecf64c3e72943ac8de5a4bfde72685666c276233
Tyler Woznica <tylerwoznica@outlook.com>
2025-08-29 17:42:37 -0700

    created finishedgood and customer pojos

---
+++ src/inventory_tracker/model/Customer.java
@@ -0,0 +1,10 @@
+package inventory_tracker.model;
+
+public class Customer {
+    private int id;
+    private String businessName;
+    private String email;
+    private String address;
+    private int phoneNumber;
+    private String contactName;
+}
---
+++ src/inventory_tracker/model/FinishedGood.java
@@ -0,0 +1,6 @@
+package inventory_tracker.model;
+
+public class FinishedGood {
+    private int id;
+    private String name;
+}
