fe6ca9bfb10e2d1951ae7ecffee92dd38e4f2df2
Tyler Woznica <tylerwoznica@outlook.com>
2025-08-29 17:45:34 -0700

    created order and orderitem classes, created static variable in customer

--- src/inventory_tracker/model/Customer.java
+++ src/inventory_tracker/model/Customer.java
@@ -1,10 +1,60 @@
 package inventory_tracker.model;
 
 public class Customer {
-    private int id;
+    public static int id;
     private String businessName;
     private String email;
     private String address;
     private int phoneNumber;
     private String contactName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
 }
+
+
---
+++ src/inventory_tracker/model/Order.java
@@ -0,0 +1,6 @@
+package inventory_tracker.model;
+
+public class Order {
+    private int id;
+    private int customerID = Customer.id;
+}
