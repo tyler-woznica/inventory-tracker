dff725a8deff99855eaae549a2910a0b48eca106
Tyler Woznica <tylerwoznica@outlook.com>
2025-08-29 17:50:52 -0700

    create OrderItem and BOMItem class

---
+++ src/inventory_tracker/model/BOMItem.java
@@ -0,0 +1,10 @@
+package inventory_tracker.model;
+
+// Bill of Materials Item
+public class BOMItem {
+    private int id;
+    // private int finishedGoodID;
+    // private in rawMaterialID;
+    private int quantityReq;
+
+}
--- src/inventory_tracker/model/OrderItem.java
+++ src/inventory_tracker/model/OrderItem.java
@@ -1,4 +1,11 @@
 package inventory_tracker.model;
 
 public class OrderItem {
+    private int id;
+    // following id's may be from another object
+    // private int order_id;
+    // private int finished_good_id;
+    // private int quantity;
+    // private double unitPrice;
+
 }
