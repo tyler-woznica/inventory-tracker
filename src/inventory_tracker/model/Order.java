4feb1e008345176df6a29f55e34ba55facdd341b
Tyler Woznica <tylerwoznica@outlook.com>
2025-08-30 09:10:52 -0700

    finished building Order class

--- src/inventory_tracker/model/Order.java
+++ src/inventory_tracker/model/Order.java
@@ -1,6 +1,48 @@
 package inventory_tracker.model;
 
 public class Order {
+
     private int id;
-    private int customerID = Customer.id;
+    private int customerID;
+    // orderDate may type may need to be changed based on SQL
+    private int orderDate;
+
+    public Order(int id, int customerID, int orderDate) {
+        this.id = id;
+        this.customerID = customerID;
+        this.orderDate = orderDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(int orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", customerID=" + customerID +
+                ", orderDate=" + orderDate +
+                '}';
+    }
 }
