82b0e89386b7c521c069ce875ba5f1a849aa958f
Tyler Woznica <tylerwoznica@outlook.com>
2025-08-30 08:55:53 -0700

    finished building Customer class and added a test class

--- src/inventory_tracker/model/Customer.java
+++ src/inventory_tracker/model/Customer.java
@@ -1,19 +1,35 @@
 package inventory_tracker.model;
 
 public class Customer {
+
+    // data will be pulled from database
     public static int id;
     private String businessName;
-    private String email;
-    private String address;
-    private int phoneNumber;
     private String contactName;
+    private String email;
+    private int phoneNumber;
+    private String street;
+    private String city;
+    private String state;
+    private int zip;
 
-    public int getId() {
+    public Customer(String businessName, String contactName, String email, int phoneNumber, String street, String city, String state, int zip) {
+        this.businessName = businessName;
+        this.contactName = contactName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public static int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public static void setId(int id) {
+        Customer.id = id;
     }
 
     public String getBusinessName() {
@@ -24,20 +40,20 @@ public class Customer {
         this.businessName = businessName;
     }
 
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public int getPhoneNumber() {
@@ -48,13 +64,52 @@ public class Customer {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getContactName() {
-        return contactName;
+    public String getStreet() {
+        return street;
     }
 
-    public void setContactName(String contactName) {
-        this.contactName = contactName;
+    public void setStreet(String street) {
+        this.street = street;
     }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "businessName='" + businessName + '\'' +
+                ", contactName='" + contactName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                '}';
+    }
+
 }
 
 
