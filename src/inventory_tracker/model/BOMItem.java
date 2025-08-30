b7c359acd1936d0aa238a7548334bf7b34760a2d
Tyler Woznica <tylerwoznica@outlook.com>
2025-08-30 09:20:25 -0700

    finished building BOMItem class

--- src/inventory_tracker/model/BOMItem.java
+++ src/inventory_tracker/model/BOMItem.java
@@ -2,9 +2,59 @@ package inventory_tracker.model;
 
 // Bill of Materials Item
 public class BOMItem {
+
+    // data retrieved from database
     private int id;
-    // private int finishedGoodID;
-    // private in rawMaterialID;
+    private int finishedGoodID;
+    private int rawMaterialID;
     private int quantityReq;
 
+    public BOMItem(int id, int finishedGoodID, int rawMaterialID, int quantityReq) {
+        this.id = id;
+        this.finishedGoodID = finishedGoodID;
+        this.rawMaterialID = rawMaterialID;
+        this.quantityReq = quantityReq;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFinishedGoodID() {
+        return finishedGoodID;
+    }
+
+    public void setFinishedGoodID(int finishedGoodID) {
+        this.finishedGoodID = finishedGoodID;
+    }
+
+    public int getRawMaterialID() {
+        return rawMaterialID;
+    }
+
+    public void setRawMaterialID(int rawMaterialID) {
+        this.rawMaterialID = rawMaterialID;
+    }
+
+    public int getQuantityReq() {
+        return quantityReq;
+    }
+
+    public void setQuantityReq(int quantityReq) {
+        this.quantityReq = quantityReq;
+    }
+
+    @Override
+    public String toString() {
+        return "BOMItem{" +
+                "id=" + id +
+                ", finishedGoodID=" + finishedGoodID +
+                ", rawMaterialID=" + rawMaterialID +
+                ", quantityReq=" + quantityReq +
+                '}';
+    }
 }
