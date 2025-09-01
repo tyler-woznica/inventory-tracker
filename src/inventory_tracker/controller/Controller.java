1e7931b4b48b6b701cd6901ba62f6e2750f5867f
Tyler Woznica <tylerwoznica@outlook.com>
2025-09-01 14:56:04 -0700

    began adding inventory options to main menu

--- src/inventory_tracker/controller/Controller.java
+++ src/inventory_tracker/controller/Controller.java
@@ -1,44 +1,85 @@
 package inventory_tracker.controller;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Controller {
     public static void main(String[] args) {
 
+        // create scanner and set userSelection to zero
         Scanner userScanner = new Scanner(System.in);
         int userSelection = 0;
 
         System.out.println("\nINVENTORY TRACKER v1\n");
 
+        // begin menu selection
         while (userSelection != 5) {
 
-            System.out.println("*** Options ***");
-            System.out.println("1. Inventory\n2. Orders\n3. Customers\n4. Import/Export\n5. Quit");
+            // try catch for any non-int entries
+            try{
+                System.out.println("MAIN MENU");
+                System.out.println("1. Inventory\n2. Orders\n3. Customers\n4. Import/Export\n5. Quit");
 
-            userSelection = userScanner.nextInt();
+                userSelection = userScanner.nextInt();
 
-            switch (userSelection) {
-                case 1:
-                    System.out.println("\n*** Inventory ***");
-                    System.out.println("1. Search\n2. Report\n3. Create Item\n4. Delete Item");
-                    break;
-                case 2:
-                    System.out.println("\n*** Orders ***");
-                    System.out.println("1. Search\n2. Create\n3. Delete\n");
-                    break;
-                case 3:
-                    System.out.println("\n*** Customers ***");
-                    System.out.println("1. Search\n2. Create\n3. Delete\n");
-                    break;
-                case 4:
-                    System.out.println("\n*** Import/Export ***");
-                    System.out.println("1. Inventory\n2. Customers\n");
-                    break;
-                case 5:
-                    System.out.println("\n*** Closing Program ***");
-                    break;
-                default:
-                    System.out.println("*** Invalid Input ***\n");
+                // main menu switch
+                switch (userSelection) {
+                    // inventory options
+                    case 1:
+                        try{
+                            System.out.println("\nINVENTORY\n");
+                            System.out.println("1. Search\n2. Report\n3. Create Item\n4. Delete Item\n5. Main Menu\n");
+                            while (userSelection != 5) {
+                                userSelection = userScanner.nextInt();
+                                switch (userSelection) {
+                                    case 1:
+                                        System.out.println("\nINVENTORY SEARCH");
+                                        break;
+                                    case 2:
+                                        System.out.println("\nINVENTORY REPORT");
+                                        break;
+                                    case 3:
+                                        System.out.println("\nCREATE ITEM");
+                                        break;
+                                    case 4:
+                                        System.out.println("\nDELETE ITEM");
+                                        break;
+                                    case 5:
+
+
+                                }
+                            }
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                        break;
+                    // orders options
+                    case 2:
+                        System.out.println("\nORDERS");
+                        System.out.println("1. Search\n2. Create\n3. Delete\n4. Main Menu\n");
+                        break;
+                    case 3:
+                        System.out.println("\nCUSTOMERS");
+                        System.out.println("1. Search\n2. Create\n3. Delete\n4. Main Menu\n");
+                        break;
+                    case 4:
+                        System.out.println("\nIMPORT/EXPORT");
+                        System.out.println("1. Inventory\n2. Customers\n3. Main Menu\n");
+                        break;
+                    case 5:
+                        System.out.println("\nCLOSING PROGRAM\n");
+                        break;
+                    default:
+                        System.out.println("INVALID INPUT: Please select an option\n");
+                }
+            // main menu catch
+            } catch (InputMismatchException e) {
+                System.out.println("INVALID INPUT: Please enter a number only\n");
+                userScanner.nextLine();
+                userSelection = 0;
             }
         }
+
+        userScanner.close();
+
     }
 }
--- src/inventory_tracker/data/MySQLConnector.java
+++ src/inventory_tracker/data/MySQLConnector.java
@@ -7,25 +7,25 @@ import java.sql.Statement;
 
 public class MySQLConnector {
 
-    private Connection connection = null;
-    private Statement statement = null;
-    private PreparedStatement preparedStatement = null;
-    private ResultSet resultSet = null;
-
-    public static void main(String[] args) {
-        MySQLConnector mySQLConnector = new MySQLConnector();
-
-        try {
-            MySQLConnector.readDataBase("JDBC Course 1", 3);
-        } catch (Exception e){
-            System.out.println("error in readDateBase()" + e.getMessage());
-            System.out.println(e.getStackTrace());
-        }
-    }
-
-    public void readDataBase {
-
-    }
+//    private Connection connection = null;
+//    private Statement statement = null;
+//    private PreparedStatement preparedStatement = null;
+//    private ResultSet resultSet = null;
+//
+//    //public static void main(String[] args) {
+//        MySQLConnector mySQLConnector = new MySQLConnector();
+//
+//        try {
+//            MySQLConnector.readDataBase("JDBC Course 1", 3);
+//        } catch (Exception e){
+//            System.out.println("error in readDateBase()" + e.getMessage());
+//            System.out.println(e.getStackTrace());
+//        }
+//    }
+//
+//    public void readDataBase {
+//
+//    }
 
 
 }
