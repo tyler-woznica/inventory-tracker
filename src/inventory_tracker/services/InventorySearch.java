29e5cb444a8fbc0d9491b2bbf4850ba78de1bde8
Tyler Woznica <tylerwoznica@outlook.com>
2025-09-02 14:26:53 -0700

    built and tested first SQL query

--- src/inventory_tracker/services/InventorySearch.java
+++ src/inventory_tracker/services/InventorySearch.java
@@ -1,4 +1,31 @@
 package inventory_tracker.services;
 
+import inventory_tracker.data.MySQLConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class InventorySearch {
+
+    public static void inventorySearch() {
+        String query = "SELECT id, name, quantity, cost FROM inventory";
+
+        try(Connection conn = MySQLConnector.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                System.out.println(
+                        rs.getInt("id") + " | " +
+                        rs.getString("name") + " | " +
+                        rs.getInt("quantity") + " | " +
+                        rs.getDouble("cost")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
